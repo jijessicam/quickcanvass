ccdf4196b8178403822a3113a0a38118a0039749
Sam <samueljacobrussell@gmail.com>
2017-03-12 21:12:01 -0400

    Add signup page and consolidate css for login module

--- quickcanvass/login/urls.py
+++ quickcanvass/login/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from login import views
 urlpatterns = [
 
-    url(r'^$', views.index, name = 'index'),
+    url(r'^login$', views.login, name = 'login'),
+    url(r'^signup$', views.signup, name = 'signup'),
 ]
--- quickcanvass/login/views.py
+++ quickcanvass/login/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
-def index(request):
+def login(request):
     return render(request, 'login/index.html')
+
+def signup(request):
+	return render(request, 'login/signup.html')
