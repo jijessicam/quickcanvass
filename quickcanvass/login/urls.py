d1b08d643478cec3a82e8f12a2e7b3b197b1e3b6
Sam <samueljacobrussell@gmail.com>
2017-03-19 03:41:20 -0400

    Pass signup form data to python and remove pyc files.

--- quickcanvass/login/urls.py
+++ quickcanvass/login/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
 
     url(r'^login$', views.login, name = 'login'),
     url(r'^signup$', views.signup, name = 'signup'),
+    url(r'^create-new-account$', views.makeaccount, name='create-new-account')
 ]
--- quickcanvass/login/views.py
+++ quickcanvass/login/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import redirect
 
 def login(request):
     return render(request, 'login/index.html')
 
 def signup(request):
 	return render(request, 'login/signup.html')
+
+@csrf_exempt
+def makeaccount(request, methods=['POST']):
+	#wow here's where we acutally upload the stuff to the db
+	data = request.POST
+	print("test", data.get('email'), data.get('passw'), data.get('isdirector'))
+	return redirect('/login/signup')
