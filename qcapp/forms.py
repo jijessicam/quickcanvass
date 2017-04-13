25731238b7478a60072f369631d1d650096a7b80
gracekturner <gracekt@princeton.edu>
2017-04-13 16:10:42 -0400

    fix deadline bug

--- qcapp/forms.py
+++ qcapp/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class CampaignForm(forms.Form):
 
    title = forms.CharField(label='Set Campaign Name', max_length=100,widget=forms.TextInput(attrs={'class' : 'form-control clean-look'}))
-   deadline = forms.DateField(label = 'Set Campaign Deadline',widget=forms.TextInput(attrs={'class' : 'form-control clean-look'}))
+   deadline = forms.DateField(label = 'Set Campaign Deadline',widget=forms.SelectDateWidget(attrs={'class' : 'form-control clean-look'}))
    description = forms.CharField(label = 'Set Campaign Details (Optional)', required = False, widget = forms.Textarea(attrs={'class' : 'form-control clean-look'}))
    contact = forms.CharField(label = 'Set Campaign Contact Information (Optional)', max_length=100, required = False,widget=forms.TextInput(attrs={'class' : 'form-control clean-look'}))
