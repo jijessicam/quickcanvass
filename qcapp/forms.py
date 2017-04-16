af9ea716dc0a7ea1268bb53edaeede296348723c
dreamer8020 <lu.ferx3@gmail.com>
2017-04-16 13:59:36 -0400

    I don't remember

--- qcapp/forms.py
+++ qcapp/forms.py
@@ -6,3 +6,6 @@ class CampaignForm(forms.Form):
    deadline = forms.DateField(label = 'Set Campaign Deadline',widget=forms.SelectDateWidget(attrs={'class' : 'form-control clean-look'}))
    description = forms.CharField(label = 'Set Campaign Details (Optional)', required = False, widget = forms.Textarea(attrs={'class' : 'form-control clean-look'}))
    contact = forms.CharField(label = 'Set Campaign Contact Information (Optional)', max_length=100, required = False,widget=forms.TextInput(attrs={'class' : 'form-control clean-look'}))
+
+class SurveyForm(forms.Form):
+	question = forms.CharField(label='Question', max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control clean-look'}))
