56eb8d9f766d1df1da0fb835968f8a38b2ad43ac
gracekturner <gracekt@princeton.edu>
2017-04-18 11:27:53 -0400

    Update urls.py

--- qcapp/urls.py
+++ qcapp/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^managerdash/(?P<netid>[A-Za-z0-9]+)/$', views.managerdash, name = 'managerdash'),
     url(r'^editcampaign/$', views.editcampaign, name = 'editcampaign'),
     url(r'^join-new-campaign/$', views.join_new_campaign, name='join_new_campaign'),
-    url(r'^editsurvey/$', views.editsurvey, name = 'editsurvey'),
+    url(r'^editsurvey/(?P<campaign_id>[0-9]+)/(?P<netid>[A-Za-z0-9]+)/$', views.editsurvey, name = 'editsurvey'),
     url(r'^fillsurvey/(?P<campaign_id>[0-9]+)/(?P<netid>[A-Za-z0-9]+)/$', views.fillsurvey, name = 'fillsurvey'),
     ## fillsurvey url not complete
 ]
