13ddec31c8075f81f59a975723b44efb9a3dffdf
gracekturner <gracekt@princeton.edu>
2017-04-19 17:04:56 -0400

    Update urls.py

--- qcapp/urls.py
+++ qcapp/urls.py
@@ -20,10 +20,10 @@ urlpatterns = [
     url(r'^accounts/callback$', django_cas_ng.views.callback, name='cas_ng_proxy_callback'),
     url(r'^volunteercampaigns/(?P<campaign_id>[0-9]+)/(?P<netid>[A-Za-z0-9]+)/$', views.volunteercampaigns, name = 'volunteercampaigns'),
     url(r'^managerdash/(?P<netid>[A-Za-z0-9]+)/$', views.managerdash, name = 'managerdash'),
-    url(r'^editcampaign/$', views.editcampaign, name = 'editcampaign'),
+    url(r'^editcampaign/(?P<netid>[A-Za-z0-9]+)/$', views.editcampaign, name = 'editcampaign'),
     url(r'^promote-to-manager/(?P<netid>[A-Za-z0-9]+)/$', views.promote_to_manager, name = 'promote_to_manager'),
     url(r'^join-new-campaign/$', views.join_new_campaign, name='join_new_campaign'),
-    url(r'^editsurvey/$', views.editsurvey, name = 'editsurvey'),
+    url(r'^editsurvey/(?P<netid>[A-Za-z0-9]+)/$', views.editsurvey, name = 'editsurvey'),
     url(r'^fillsurvey/(?P<campaign_id>[0-9]+)/(?P<netid>[A-Za-z0-9]+)/$', views.fillsurvey, name = 'fillsurvey'),
     ## fillsurvey url not complete
 ]
